fda71eff9e27bf125032ecea461c07aba580cd28
Mark Pelayo <mark.pelayo014@gmail.com>
2024-12-02 10:21:22 +0800

    minor changes

--- Problem Set 2/caesar.c
+++ Problem Set 2/caesar.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool digit(string n);
+bool digit(string k); // confuse how it passes, but yeah.
 string caesar_algo(string p, int k);
 
 int main(int argc, string argv[])
