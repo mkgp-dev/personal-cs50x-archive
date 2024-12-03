88df62b961ea0f8b9380780bb10165d9d0f2efc6
Mark Pelayo <mark.pelayo014@gmail.com>
2024-12-04 02:58:54 +0800

    Submitted at 4th of December, 2024

---
+++ Problem Set 4/volume.c
@@ -0,0 +1,16 @@
+int main(int argc, char *argv[])
+{
+    // Header fread, fwrite
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // Read and write in while loop
+    int16_t buffer;
+
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+}
